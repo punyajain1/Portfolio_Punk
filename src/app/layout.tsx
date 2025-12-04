84c872378e6e25aad807544232dd8e1c845619ca
punyajain1 <punyajain50@gmail.com>
2025-12-04 16:34:12 +0530

    added anytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClarityAnalytics from "@/components/ClarityAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <ClarityAnalytics />
       </body>
     </html>
   );
